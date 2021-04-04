640a020cc08b877ff5927d5acad72fcd925e4bde
github username <github email address>
2021-04-04 15:05:23 -0400

    created a simple todo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const todoArray = [];
+
+  const submitButton = document.getElementById('submitTodoButton');
+
+  //Returns the UL where the list will be placed
+  const tasksArea = document.getElementById('tasks');
+  
+
+  submitButton.addEventListener('click', function(e){
+    const taskInput = document.getElementById('new-task-description');
+    const createLi = document.createElement('li');
+
+    e.preventDefault();
+
+    if (taskInput.value === ''){
+      return alert('Nothing was inputted');
+    } else {
+      todoArray.push(taskInput.value);
+      
+      
+      tasksArea.appendChild(createLi);
+      createLi.innerHTML = taskInput.value;
+
+      taskInput.value = '';
+    }
+    
+    
+  })
+
+
+
+
+
 });
